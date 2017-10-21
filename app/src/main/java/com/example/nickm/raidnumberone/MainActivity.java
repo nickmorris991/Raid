0e1a6ccf9529e8905a530dac4b07d6b6de652041
nickmorris991 <nickmorris25@outlook.com>
2017-10-21 18:33:35 -0400

    Initial commit

---
+++ app/src/main/java/com/example/nickm/raidnumberone/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.nickm.raidnumberone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton doctorImageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        doctorImageButton = (ImageButton) findViewById(R.id.imageButton4);
+        doctorImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, NewActivity.class);
+                startActivity(intentLoadNewActivity);
+            }
+    }   }};
+
+}
